4f06404f10119abd796a7b6ba73d9775641faf99
Mike Woods <mwoods@figure.com>
2022-07-11 17:43:07 -0400

    Add vault helper

---
+++ vault/vault-prod.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [[ -z "$OKTA_EMAIL_ADDR" ]]; then
+  echo "Define OKTA_EMAIL_ADDR first"
+  exit 1
+fi
+
+# See https://www.vaultproject.io/docs/commands#environment-variables
+unset VAULT_TOKEN
+export VAULT_ADDR=https://vault.figure.com:8200
+export VAULT_TOKEN=$(vault login -method=okta username=$OKTA_EMAIL_ADDR -format=json | jq -r '.auth.client_token')
+
+vault_ls() {
+  vault kv list /vc_secrets/$1
+}
+
+alias vault-ls=vault_ls
+
